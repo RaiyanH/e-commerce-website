c96e46ff6aa3f3c14e51b4e6528b13706ee8008d
Qays Mulla <qaysmulla@Tasneems-MacBook.local>
2022-09-12 11:14:48 +0100

    Initial commit

---
+++ src/main/java/com/fdmgroup/group/FinalGroupProjectQrkApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalGroupProjectQrkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalGroupProjectQrkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdmgroup/group/FinalGroupProjectQrkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalGroupProjectQrkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
